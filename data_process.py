040b4aba4d5165831a6cd5b02b288732a27e93c6
fuku-tn <phuc.nguyen131198@gmail.com>
2025-02-28 11:03:52 +0700

    Optimize for large datasets with SQL query and chunking

--- data_process.py
+++ data_process.py
@@ -3,20 +3,18 @@ from langchain_experimental.agents.agent_toolkits.pandas.base import create_pand
 import os
 from dotenv import load_dotenv
 from database import stream_data_from_table
-from llm import load_llm
+from src.modeling.llm import load_llm
 
 load_dotenv()
 openai_api_key = os.getenv("OPENAI_API_KEY")
 
-def process_data_and_analyze(engine, schema, tables, chunk_size=5000, query=None, model_name="gpt-4o-mini"):
+def process_data_and_analyze(engine, schema, tables, chunk_size=50000, custom_query=None, query=None, model_name="gpt-4o-mini"):
     df = pd.DataFrame()
     for table in tables:
-        for chunk in stream_data_from_table(engine, schema, table, chunk_size):
+        for chunk in stream_data_from_table(engine, schema, table, chunk_size, custom_query):
             df = pd.concat([df, chunk], ignore_index=True)
-            if len(df) > 10000:  # Giới hạn để tránh quá tải
-                break
+            break  # Chỉ lấy chunk đầu tiên trong lần gọi này
     if query and not df.empty:
-        # Sử dụng load_llm với model_name được truyền từ Streamlit
         llm = load_llm(model_name=model_name, openai_api_key=openai_api_key)
         da_agent = create_pandas_dataframe_agent(
             llm=llm,
